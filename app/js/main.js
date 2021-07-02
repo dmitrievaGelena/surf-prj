e5cc801fe4fc4d4b103bb3825534a71de29e29f1
Gelena Dmitrieva <dmitrievaGelenaOneTwo@gmail.com>
2021-07-02 22:44:27 +0300

    add sliders blocks

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,21 @@
-$(function() {
-    $('.product__inner-slider').slick({
-        dots: false,
-        infinite: true,
-        speed: 700,
-        fade: true,
-        cssEase: 'linear',
-        arrow: true,
-        prevArrow: '<button class="arrow prev-arrow"><span class="icon-angle-left"></span></button>',
-        nextArrow: '<button class="arrow next-arrow"><span class="icon-angle-right"></span></button>'
-      });
+$(function () {
+  $('.product__inner-slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 700,
+    fade: true,
+    cssEase: 'linear',
+    arrow: true,
+    prevArrow: '<button class="arrow prev-arrow"><span class="icon-angle-left"></span></button>',
+    nextArrow: '<button class="arrow next-arrow"><span class="icon-angle-right"></span></button>'
+  });
+
+  $('.product__carousel-contents').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    speed: 600,
+    prevArrow: '<button class="arrow prev-arrow"><span class="icon-angle-left"></span></button>',
+    nextArrow: '<button class="arrow next-arrow"><span class="icon-angle-right"></span></button>'
+  });
 });
