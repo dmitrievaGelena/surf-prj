039ff9d4e7abcf83b43170d54558e833d0faa6d4
Gelena Dmitrieva <dmitrievaGelenaOneTwo@gmail.com>
2021-07-16 23:10:51 +0300

    create apparel.html, more.html, shop.html

--- app/js/main.js
+++ app/js/main.js
@@ -38,6 +38,64 @@ $(function () {
       }
     }
   });
+
+
+  $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: false,
+    arrows: true,
+    // centerMode: true,
+    focusOnSelect: true,
+    prevArrow: '<button class="arrow prev-arrow"><span class="lnr lnr-chevron-left"></span></button>',
+    nextArrow: '<button class="arrow next-arrow"><span class="lnr lnr-chevron-right"></span></button>'
+  });
+
+  $('.more-slider-box').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows: true,
+    speed: 700,
+    prevArrow: '<button class="arrow prev-arrow"><span class="lnr lnr-chevron-left"></span></button>',
+    nextArrow: '<button class="arrow next-arrow"><span class="lnr lnr-chevron-right"></span></button>'
+  });
+
+  $('.aside__category-list li').on('click', function () {
+    $('.aside__options-menu').slideDown();
+    $('.aside__category-list li').removeClass('actives');
+    $(this).addClass('actives');
+  });
+  $('.options__menu-list li').on('click', function () {
+    $('.options__menu-list li').removeClass('actives');
+    $(this).addClass('actives');
+  });
+  $('.options__size-chart span').on('click', function () {
+    $('.options__size-chart span').removeClass('active');
+    $(this).addClass('active');
+  });
+
+  $('.icon-th-list').on('click', function () {
+    $('.icon-th-large').removeClass('active');
+    $('.icon-th-list').addClass('active');
+    $('.product__item').addClass('active');
+  });
+
+  $('.icon-th-large').on('click', function () {
+    $('.icon-th-large').addClass('active');
+    $('.icon-th-list').removeClass('active');
+    $('.product__item').removeClass('active');
+  });
+
+
   owl.on('mousewheel', '.owl-stage', function (e) {
     if (e.deltaY > 0) {
       owl.trigger('next.owl');
@@ -47,5 +105,32 @@ $(function () {
     e.preventDefault();
   });
 
+  $("#rateYo").rateYo({
+    rating: 3.6,
+    ratedFill: "#000",
+    readOnly: true,
+    spacing: '5px',
+    starWidth: "15px"
+  });
+  $('.more__tabs .tab').on('click', function (event) {
+    var id = $(this).attr('data-id');
+    $('.wrapper').find('.tab-item').removeClass('active-tab').hide();
+    $('.wrapper .tabs').find('.tab').removeClass('active');
+    $(this).addClass('active');
+    $('#' + id).addClass('active-tab').fadeIn();
+    return false;
+  });
+
   new WOW().init();
 });
+
+
+//сделать фде для изображений
+//минифицировать изображения 
+//повторяющиееся цвета в vars 
+//анимация
+// поменять текст везде на строчной
+// убрать прыжки во время клика по элементам
+// добавить сортировку по колличеству элементов, по id
+//заанимировать теги по клику. Можно выбрать несколько сразу и под них будут сортироваться продукты
+//подписать alt у img сделать счетчик колличества установить максимальное колл дя input 
